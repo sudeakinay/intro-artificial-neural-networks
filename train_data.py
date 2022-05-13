ccb583c5090499f3140eb0956a9e8894f940fb82
sudeakinay <nesrinakinay.s@gmail.com>
2022-05-13 18:38:34 +0300

    updated train_data.py

--- train_data.py
+++ train_data.py
@@ -1,18 +1,31 @@
 #############################  MY NEURAL NETWORK MODEL (TRAIN) #############################
+from distutils.log import fatal
 import numpy as np                      # allows scienftific computing                                   
 import joblib                           # allows load models
 
 class NeuralNetwork:
-    def __init__(self, learning_rate, iterations):
-        self.weights = np.array([np.random.randn(), np.random.randn(), np.random.randn(), np.random.randn()])
-        self.bias = np.random.randn()
-
+    def __init__(self, learning_rate, iterations, input_dimension, hidden_layers):
         self.learning_rate = learning_rate
         self.iterations = iterations
         if np.any(learning_rate <= 0):
             raise ValueError("learning_rate must be greater than zero")
         if  np.any(iterations <= 0):
             raise ValueError("number of iteration must be greater than zero")
+
+        self.input_dimension = input_dimension
+        self.hidden_layers = hidden_layers
+        self.weights = np.array([np.random.randn(input_dimension)])
+        self.bias = np.random.randn()
+
+        self.weights_input_to_hidden = np.random.uniform(-1, 1, (input_dimension, hidden_layers))
+        self.weights_hidden_to_output = np.random.uniform(-1, 1, (hidden_layers))
+
+        pre_hidden = np.zeros(hidden_layers)
+        post_hidden = np.zeros(hidden_layers)
+        self.pre_hidden = pre_hidden
+        self.post_hidden = post_hidden
+        LR = 1
+        self.LR = LR
        
     def _LReLU(self, x):
         x = np.array(x)
@@ -57,13 +70,27 @@ class NeuralNetwork:
         return self.weights, self.bias
 
     def train(self, input_vectors, targets, iterations):
-        total_errors = []
+
         for current_iteration in range(iterations) :
-            random_data_index = np.random.randint(len(input_vectors))
-            input_vector = input_vectors[random_data_index] 
-            target = targets[random_data_index]
-            derror_dbias, derror_dweights = self._computer_gradients(input_vector, target)
-            self._update_parameters(derror_dbias, derror_dweights)
+            for sample in range(len(input_vectors[:, 0])):
+                for node in range(self.hidden_layers):
+                    self.pre_hidden[node] = np.dot(input_vectors[sample, :], self.weights_input_to_hidden, node)
+                    post_hidden = self._LReLU(self.pre_hidden[node])
+
+                pre_hidden_O = np.dot(post_hidden, self.weights_hidden_to_output)
+                post_hidden_O = self._LReLU(pre_hidden_O)     
+                fatal_error = post_hidden_O - targets[sample]
+
+            for hidden_node in range(self.hidden_layers):
+                S_error = fatal_error * self._LReLU_deriv(pre_hidden_O)
+                gradient_hidden_to_output = S_error * post_hidden[hidden_node]
+
+                for input_node in range(input_dimension):
+                    input_value = input_vectors[sample, input_node]
+                    gradient_input_to_hidden = S_error * self.weights_hidden_to_output[hidden_node] * self._LReLU_deriv(self.pre_hidden[hidden_node] * input_value)
+
+                    self.weights_input_to_hidden[input_node, hidden_node] -= self.LR * gradient_input_to_hidden
+                self.weights_hidden_to_output[hidden_node] -= self.LR * gradient_hidden_to_output
 
             if current_iteration % 100 == 0: 
                 total_error = 0
@@ -72,14 +99,13 @@ class NeuralNetwork:
                     target = targets[data_instance_index]
                     prediction = self.predict(data_point)       
                     error = np.square(prediction - target)      
-                    total_error = total_error + error
-                total_errors.append(total_error)      
+                    total_error = total_error + error   
                 
-            for i in total_errors:
+            for i in total_error:
                 if i <= 10^(-5):
                     break  
 
-        return total_errors, self.weights, self.bias
+        return total_error, self.weights, self.bias
 
     def test_function(self, test_data, final_weights, final_bias):
         test_data_pred = []
@@ -112,6 +138,10 @@ class NeuralNetwork:
 
 learning_rate = 0.01
 iterations = 100000
-model = NeuralNetwork(learning_rate, iterations)
+input_dimension = 1
+hidden_layers = 5
+
+
+model = NeuralNetwork(learning_rate, iterations, input_dimension, hidden_layers)
 joblib.dump(model, "my_first_model")
 loaded_model = joblib.load("my_first_model")
